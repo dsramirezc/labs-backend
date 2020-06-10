c953acec3e398b2e3cbdc7fa8e555f0c9d0f8d1d
stephan <dsramirezc@unal.edu.co>
2020-06-10 04:02:37 -0500

    success.cpp

---
+++ success.cpp
@@ -0,0 +1,13 @@
+///By Stephan Ramirez dsramirezc@unal.edu.co
+#include<bits/stdc++.h>
+#define ms(a,v) memset(a,v,sizeof a)
+#define ll long long
+using namespace std;
+int main(){
+	#ifdef LOCAL
+		freopen("in.txt","r",stdin);
+		//freopen("out.txt","w",stdout)	;
+	#endif
+	return 0;
+}
+
